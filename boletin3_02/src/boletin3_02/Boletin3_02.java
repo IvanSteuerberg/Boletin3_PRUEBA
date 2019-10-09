7c4cefc1c0ad4daed27527e7889b81126404629a
Ivan Steuerberg <irodriguezsteuerberg@danielcastelao.org>
2019-10-09 13:41:12 +0200

    prueba 2

---
+++ boletin3_02/src/boletin3_02/Boletin3_02.java
@@ -0,0 +1,14 @@
+package boletin3_02;
+
+//@author irodriguezsteuerberg
+ 
+public class Boletin3_02 {
+
+    public static void main(String[] args) {
+    
+System.out.println("Hola");
+    
+    
+    }
+ 
+}
